aef54511e5305771002232e13e304e646345c15a
Your Name <freeluck.v.i.p999@gmail.com>
2026-01-15 22:41:54 +0000

    todo: will add os specific tasks, and remember to re-add terminal clear after every loop

--- portable.py
+++ portable.py
@@ -2,12 +2,17 @@ import platform
 
 key = "e" 
 while key != "q":
-    print("\033[2J\033[1;1H")
     os = platform.platform()
     print(os)
-    key = input()
+    key = input("Welcome to Ultra Portable Toolkit (UPTKIT)! Press q to quit, otherwise press any key to continue:")
     if key == "q":
         print("exited sucessfully.")
         exit()
+    else:
+        system = platform.system()
+        print(system)
+
+
+
         
 
