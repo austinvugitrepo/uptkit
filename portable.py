9bc92db6363e1e9c7aebcd4c28744345b26ff608
Your Name <freeluck.v.i.p999@gmail.com>
2026-01-16 19:40:17 +0000

    updated os portion, added sleep for delay and cleared terminal once again

--- portable.py
+++ portable.py
@@ -1,9 +1,10 @@
 import platform
 import shutil
+import time
 
 key = "e" 
 while key != "q":
-    os = platform.platform()
+    os = platform.platform() #entire OS stack
     print(os)
     key = input("Welcome to Ultra Portable Toolkit (UPTKIT)! Press q to quit, otherwise press any key to continue:")
     if key == "q":
@@ -17,11 +18,18 @@ while key != "q":
                     print(i)
 
         elif system == "OpenBSD":
-            print("this is OpenBSD system")
+            if shutil.which("pkg_add"):
+                print("This system uses pkg_add")
         elif system == "Darwin":
-            print("this is macOS system")
+            if shutil.which("brew"):
+                print("this system uses brew")
+
         elif system == "Windows":
-            print("this is Windows system")
+            if shutil.which("winget"):
+                print("this system uses winget")
+        time.sleep(0.5) #in seconds
+        print("\033[2J\033[1;1H")
+
 
 
 
